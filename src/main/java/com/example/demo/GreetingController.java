be54dad999acc572b8ca32051e432e9c2cfe90df
Phil-Shepard <Philipp-74@yandex.ru>
2024-02-01 17:42:44 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("someInformation", "Hello, it's a first good program by Phil Shepard!");
+        return "main";
+    }
+
+}
